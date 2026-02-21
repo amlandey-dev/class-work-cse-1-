16bbec7056bcbba2c75e7cd27104fe9bedcbac1e
GHOST <g3175318@gmail.com>
2026-02-22 02:01:40 +0530

    Create largest among two numbers using ternary operator.c
    largest among two numbers
    using ternary operator.

---
+++ largest among two numbers using ternary operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main (){
+    int a,b,c;
+   //clrscr (for turbo c++ compiler)
+   printf("Enter 1st number:");
+   scanf("%d",&a);
+   printf("Enter 2nd number:");
+   scanf("%d",&b);
+   c=(a>b)?a:b;
+   printf("The Largest Number between %d and %d is %d",a,b,c);
+
+//getch(for turbo c++ compiler)
+}
+
+
+
+
+/*Question--- write a 'c' program to find the largest among two numbers
+using ternary operator.*/
