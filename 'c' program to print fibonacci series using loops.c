34998164040c0d880d6864059f7064542de930d7
GHOST <g3175318@gmail.com>
2026-02-26 10:48:14 +0530

    'c' program to print fibonacci series using loops.c
    c  program to print fibonacci series using loops.c

--- 'c' program to print fibonacci series using loops.c
+++ 'c' program to print fibonacci series using loops.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 void main(){
-    int n,a=0,b=1,i,c;
+    int n,a=0,b=1,c,i;
     //clrscr() (for turbo c++ users)
     printf("Enter any number to get fibonacci series:");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
       printf("%d",a);
+      printf("\n");
       c=a+b;
       a=b;
       b=c;
+    }
       //getch() (for turbo c++ users)
     }
-}
+
 
 //Write a 'c' program to print fibonacci series using loops.
