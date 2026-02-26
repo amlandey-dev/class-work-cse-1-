29eed90c2865bb73cae8d243c15f760591af9b58
GHOST <g3175318@gmail.com>
2026-02-26 10:21:35 +0530

    Create 'c' program to print the series:(n,n-1,n-2,......,3,2,1).c
    'c' program to print the series:(n,n-1,n-2,......,3,2,1).

---
+++ 'c' program to print the series:(n,n-1,n-2,......,3,2,1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    int i,n;
+    //clrscr() (for turbo c++ users)
+    printf("Enter any number to start the series:");
+    scanf("%d",&n);
+    printf("\nThe series is:");
+    for(i=n;i>=1;i--){
+      printf("%d",i);
+      printf("\n");
+    }
+      //getch() (for turbo c++ users)
+    }
+
+
+//Write a 'c' program to print the series:(n,n-1,n-2,......,3,2,1).
