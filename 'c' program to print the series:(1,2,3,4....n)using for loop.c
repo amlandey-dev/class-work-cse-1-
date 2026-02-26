979895d7ac00ec918cc70e71c1a0fb7b97fcb541
GHOST <g3175318@gmail.com>
2026-02-26 09:50:34 +0530

    Create 'c' program to print the series:(1,2,3,4....n)using for loop.c
    'c' program to print the series:(1,2,3,4....n)using for loop.

---
+++ 'c' program to print the series:(1,2,3,4....n)using for loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    int i=1,n;
+    //clrscr() (for turbo c++ users)
+    printf("The series of n end at number:");
+    scanf("%d",&n);
+    printf("\nThe series is:");
+    for(i=1;i<=n;i++){
+      printf("%d",i);
+    }
+      //getch() (for turbo c++ users)
+    }
+
+
+//Write a 'c' program to print the series:(1,2,3,4....n)using for loop.
