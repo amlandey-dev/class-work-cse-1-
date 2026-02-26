95766b259b4aa4fa7c5bad8fdd6548c2954740c7
GHOST <g3175318@gmail.com>
2026-02-26 09:46:10 +0530

    Create 'c' program to print the series:(1,2,3,4....n)using do-while loop.c
    'c' program to print the series:(1,2,3,4....n)using do-while loop.

---
+++ 'c' program to print the series:(1,2,3,4....n)using do-while loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    int i=1,n;
+    //clrscr() (for turbo c++ users)
+    printf("The series of n end at number:");
+    scanf("%d",&n);
+    printf("\nThe series is:");
+    do{
+      printf("%d",i);
+      i++;
+    }while(i<=n);
+      //getch() (for turbo c++ users)
+    }
+
+
+//Write a 'c' program to print the series:(1,2,3,4....n)using do-while loop.
