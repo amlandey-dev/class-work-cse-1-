9f488a705d94c8311e03895f6fdc529ad98675f1
GHOST <g3175318@gmail.com>
2026-02-26 09:43:47 +0530

    Create 'c' program to print the series:(1,2,3,4....n)using while loop.c
    'c' program to print the series:(1,2,3,4....n)using while loop.

---
+++ 'c' program to print the series:(1,2,3,4....n)using while loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    int i=1,n;
+    //clrscr() (for turbo c++ users)
+    printf("The series of n end at number:");
+    scanf("%d",&n);
+    printf("\nThe series is:");
+    while(i<=n){
+      printf("%d",i);
+      i++;
+    }
+      //getch() (for turbo c++ users)
+    }
+
+
+//Write a 'c' program to print the series:(1,2,3,4....n)using while loop.
